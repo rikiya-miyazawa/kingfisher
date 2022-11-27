588925549164af2f8d9180e891f16a7368710c07
rikiya miyazawa <rky.weed.rss@gmail.com>
2022-11-27 16:28:22 +0900

    モデルを作成

---
+++ app/models/king_fisher.rb
@@ -0,0 +1,2 @@
+class KingFisher < ApplicationRecord
+end
---
+++ test/models/king_fisher_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class KingFisherTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
