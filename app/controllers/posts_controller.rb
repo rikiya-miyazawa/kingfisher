a077eb4f44ebb70fd9528888baddf3b1dbe0b4e4
rikiya miyazawa <rky.weed.rss@gmail.com>
2022-11-29 11:55:31 +0900

    feat:つぶやき削除機能の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:edit, :update]
+  before_action :set_post, only: [:edit, :update, :destroy]
 
   def index
     @posts = Post.all
@@ -30,6 +30,8 @@ class PostsController < ApplicationController
   end
 
   def destroy
+    @post.destroy
+    redirect_to posts_path, notice:"消しちゃたー"
   end
 
   private
