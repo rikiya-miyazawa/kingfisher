aa14c3db215fe8e221c4a4f864956caf581ab81c
rikiya miyazawa <rky.weed.rss@gmail.com>
2022-11-29 11:32:33 +0900

    feat:編集&更新機能の追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,9 +18,16 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to posts_path, notice: "編集完了!!"
+    else
+      render :edit
+    end
   end
 
   def destroy
